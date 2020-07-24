0ae23c7ae76c4fb8f5689665ea0e3fb66fa7b8c5
Sherzod <fortuneman@list.ru>
2020-07-24 21:09:50 +0500

    Proxies delete action is done

--- routes/web.php
+++ routes/web.php
@@ -33,20 +33,24 @@ Route::group( ['namespace' => 'Api', 'prefix' => 'api'],function () {
     //Соответсвует урл /api/settings/
     Route::group(['namespace' => 'Settings', 'prefix' => 'settings'], function () {
 
-        //Получение списка проксей /api/settings/proxies/
-        Route::get('proxies', 'ProxiesController@index' );
+        //Работа с проксями
+        Route::group(['prefix' => 'proxies'], function () {
 
-        //Активация деактивация прокси /api/settings/proxies/{id}/status/
-        Route::patch('proxies/{id}/status/', 'ProxiesController@setStatus' )->where('id', '[1-9][0-9]*');
+            //Получение списка проксей /api/settings/proxies/
+            Route::get('/', 'ProxiesController@index' );
 
-        //Активация деактивация многих проксей /api/settings/proxies/status/
-        Route::patch('proxies/status/', 'ProxiesController@setMultiplyStatuses' );
+            //Активация деактивация прокси /api/settings/proxies/{id}/status/
+            Route::patch('/{id}/status/', 'ProxiesController@setStatus' )->where('id', '[1-9][0-9]*');
 
-        //Удаление прокси /api/settings/proxies/{id}/
-        Route::delete('proxies/{id}/', 'ProxiesController@destroy' )->where('id', '[1-9][0-9]*');
+            //Активация деактивация многих проксей /api/settings/proxies/status/
+            Route::patch('/status/', 'ProxiesController@setMultiplyStatuses' );
 
-        //Активация деактивация многих проксей /api/settings/proxies/
-        Route::delete('proxies/', 'ProxiesController@destroyMultiply' );
+            //Удаление прокси /api/settings/proxies/{id}/
+            Route::delete('/{id}/', 'ProxiesController@destroy' )->where('id', '[1-9][0-9]*');
+
+            //Удаление проксей /api/settings/proxies/
+            Route::delete('/', 'ProxiesController@destroyMultiply' );
+        });
 
     });
 });
