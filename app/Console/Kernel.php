1e23ebf36096585fdc0953884f89cdbd87c41bda
Sherzod <fortuneman@list.ru>
2020-07-30 21:18:57 +0500

    domens check changed

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -29,7 +29,7 @@ class Kernel extends ConsoleKernel
         $schedule->call(function () {
             $domainChecker = new DomainCheckerService();
             $domainChecker->check();
-        })->everyThirtyMinutes();
+        })->everyMinute();
     }
 
     /**
