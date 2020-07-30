e047a395cb21df4b596e77074abe8a83acd0805d
Sherzod <fortuneman@list.ru>
2020-07-31 01:15:08 +0500

    Vue-ladda added

--- resources/js/components/pages/Messages/SmsMessages.vue
+++ resources/js/components/pages/Messages/SmsMessages.vue
@@ -6,84 +6,48 @@
       <div>
         <VueElementLoading :active="isLoading" spinner="bar-fade-scale" color="var(--primary)"/>
 
+        <b-card title="Фильтр" class="main-card mb-2">
+
+          <p>With supporting text below as a natural lead-in to additional content.</p>
+            <p class="mb-0">Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled.</p>
+          <div class="d-block text-right card-footer">
+            <b-button class="btn btn-shadow btn-hover-shine btn-transition btn-secondary" >Сбросить фильтр</b-button>
+            <vue-ladda data-style="zoom-out" button-class="btn btn-primary btn-shadow btn-hover-shine btn-transition" :loading="filterButton.loading" :progress="filterButton.progress" @click="filter()">Применить</vue-ladda>
+          </div>
+        </b-card>
+
+
         <div class="container-fluid">
           <div class="row">
-            <div class="col-sm-auto">
-              <b-button size="sm" class="mr-2 mb-2 btn-shadow btn-hover-shine btn-transition" variant="primary" @click="selectAll()">
-                Выбрать все
-              </b-button>
-              <b-dropdown dropup no-flip text="Действия над выбранными" class="mb-2 mr-2" variant="primary" ref="dropdown0" :disabled="checkedItemsCount==0" >
-                <div class="dropdown-menu-header">
-                  <div class="dropdown-menu-header-inner bg-secondary">
-                    <div class="menu-header-image opacity-5 dd-header-bg-2"></div>
-                    <div class="menu-header-content"><h6 class="menu-header-title">Действия</h6></div>
-                  </div>
-                </div>
-                <button type="button" tabindex="0" class="dropdown-item" @click="setProxiesStatus(null, 1)">Активировать</button>
-                <button type="button" tabindex="1" class="dropdown-item" @click="setProxiesStatus(null, 0)">Деактивировать</button>
-                <button type="button" tabindex="2" class="dropdown-item">Проверить</button>
-                <button type="button" tabindex="2" class="dropdown-item text-danger" @click="onDeleteProxies()">Удалить</button>
-              </b-dropdown>
-            </div>
             <div class="col" align="left">
               <div>
                 <h5>Количество записей: {{itemsTotalCount}}</h5>
               </div>
             </div>
             <div class="col-md-auto">
-              <button type="button" @click="showAddProxiesModal=true" class="btn-shadow d-inline-flex align-items-center btn btn-success" >
-                <font-awesome-icon class="mr-2" icon="plus"/>
-                Добавить прокси
-              </button>
             </div>
             <div class="col-md-auto">
-              <b-form-select v-model="itemsPerPage" :options="itemsPerPageOptions" @change="page = 1; getProxies()"></b-form-select>
+              <b-form-select v-model="itemsPerPage" :options="itemsPerPageOptions" @change="page = 1; getSmsList()"></b-form-select>
             </div>
           </div>
         </div>
 
         <b-table striped bordered outlined hover fixed :items="items" :fields="fields">
 
-          <template v-slot:cell(checkbox_field)="data">
-            <b-form-checkbox v-model="data.item.checked"></b-form-checkbox>
+          <template v-slot:cell(thematics_name)="data">
+            {{data.item.advertising_campaign.thematics.name}}
           </template>
 
-          <template v-slot:cell(type)="data">
-            <div>{{proxyTypes[data.item.type]}}</div>
+          <template v-slot:cell(advertising_campaign_name)="data">
+            {{data.item.advertising_campaign.name}}
           </template>
 
-          <template v-slot:cell(status)="data">
-            <div v-show="data.item.status==1" class="text-success">Да</div>
-            <div v-show="data.item.status==0" class="text-danger">Нет</div>
+          <template v-slot:cell(user_name)="data">
+            {{data.item.advertising_campaign.user.name}}
           </template>
-
-          <template v-slot:cell(is_banned)="data">
-            <div v-show="data.item.is_banned==1" class="text-danger">Да</div>
-            <div v-show="data.item.is_banned==0" class="text-success">Нет</div>
-          </template>
-
-          <template v-slot:cell(operations)="data">
-
-            <b-dropdown dropup no-flip text="Действия" class="mb-2 mr-2" variant="primary" block :ref="'dropdown_'+data.item.id">
-              <div class="dropdown-menu-header">
-                <div class="dropdown-menu-header-inner bg-secondary">
-                  <div class="menu-header-image opacity-5 dd-header-bg-2"></div>
-                  <div class="menu-header-content"><h6 class="menu-header-title">Действия</h6></div>
-                </div>
-              </div>
-              <button type="button" tabindex="0" class="dropdown-item" v-show="data.item.status==0" @click="setProxiesStatus(data.item.id, 1)">Активировать</button>
-              <button type="button" tabindex="1" class="dropdown-item" v-show="data.item.status==1" @click="setProxiesStatus(data.item.id, 0)">Деактивировать</button>
-              <button type="button" tabindex="2" class="dropdown-item" v-show="data.item.check_state==0">Проверить</button>
-              <button type="button" tabindex="1" class="dropdown-item text-primary" @click="onEditProxy(data.item)">Редактировать</button>
-              <button type="button" tabindex="1" class="dropdown-item text-danger" @click="onDeleteProxies(data.item.id, 0)">Удалить</button>
-            </b-dropdown>
-
-          </template>
-
 
           <template v-slot:table-colgroup="scope">
             <col style="width: 30px">
-            <col style="width: 50px">
             <col>
             <col>
             <col>
@@ -92,7 +56,6 @@
             <col>
             <col>
             <col>
-            <col style="width: 120px">
           </template>
 
         </b-table>
@@ -100,35 +63,15 @@
         <div class="container-fluid">
           <div class="row">
             <div class="col-md-auto">
-              <b-button size="sm" class="mr-2 mb-2 btn-shadow btn-hover-shine btn-transition" variant="primary" @click="selectAll()">
-                Выбрать все
-              </b-button>
-
-              <b-dropdown dropup no-flip text="Действия над выбранными" class="mb-2 mr-2" variant="primary" ref="dropdown1" :disabled="checkedItemsCount==0" >
-                <div class="dropdown-menu-header">
-                  <div class="dropdown-menu-header-inner bg-secondary">
-                    <div class="menu-header-image opacity-5 dd-header-bg-2"></div>
-                    <div class="menu-header-content"><h6 class="menu-header-title">Действия</h6></div>
-                  </div>
-                </div>
-                <button type="button" tabindex="0" class="dropdown-item" @click="setProxiesStatus(null, 1)">Активировать</button>
-                <button type="button" tabindex="1" class="dropdown-item" @click="setProxiesStatus(null, 0)">Деактивировать</button>
-                <button type="button" tabindex="2" class="dropdown-item">Проверить</button>
-                <button type="button" tabindex="2" class="dropdown-item text-danger" @click="onDeleteProxies()">Удалить</button>
-              </b-dropdown>
             </div>
             <div class="col-md-auto">
-              <button type="button" @click="showAddProxiesModal=true" class="btn-shadow d-inline-flex align-items-center btn btn-success">
-                <font-awesome-icon class="mr-2" icon="plus"/>
-                Добавить прокси
-              </button>
             </div>
             <div class="col">
 
             </div>
 
             <div class="col-md-auto">
-              <v-pagination v-model="page" @input="getProxies()" :length="pagesCount" :total-visible="10"></v-pagination>
+              <v-pagination v-model="page" @input="getSmsList()" :length="pagesCount" :total-visible="10"></v-pagination>
             </div>
           </div>
         </div>
@@ -149,29 +92,33 @@
   import { faStar, faPlus } from '@fortawesome/free-solid-svg-icons'
   import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
   library.add( faStar, faPlus );
+
+  import VueLadda from '../../../assets/components/ladda-loading/src/vue-ladda'
+
+  const duration = 2000;
+
   export default {
     components: {
       PageTitle,
       VueElementLoading,
       vSelect,
       'font-awesome-icon': FontAwesomeIcon,
+      VueLadda
     },
     data: () => ({
       heading: 'База сообщений',
       subheading: 'SMS сообщения',
       icon: 'pe-7s-home icon-gradient bg-warm-flame',
       fields: [
-        {key:'checkbox_field', label:''},
         {key:'id', label:'ID'},
-        {key:'type', label:'Тип'},
-        {key:'ip', label:'IP'},
-        {key:'port', label:'Порт'},
-        {key:'login', label:'login'},
-        {key:'password', label:'password'},
-        {key:'status', label:'Активный'},
-        {key:'busy_by_task_id', label:'Занят под задание'},
-        {key:'is_banned', label:'Забанен'},
-        {key:'operations', label:'Операции'},
+        {key:'text', label:'Текст сообщения'},
+        {key:'thematics_name', label:'Тематика'},
+        {key:'advertising_campaign_name', label:'Рекламная компания'},
+        {key:'sent_count', label:'Кол. отправленных'},
+        {key:'clicks_count', label:'Кол. переходов'},
+        {key:'used_simcards_count', label:'Кол. исп. симкарт'},
+        {key:'created_at', label:'Дата создания'},
+        {key:'user_name', label:'Владелец'},
       ],
       itemsPerPageOptions: [
         {text: 'Показать по 10',  value: '10'},
@@ -189,17 +136,10 @@
       page: 1,
       pagesCount :1,
       itemsTotalCount : 0,
-      showAddProxiesModal: false,
-      showEditProxyModal: false,
-      proxyToEdit : {
-        id: null,
-        ip: '',
-        port:'',
-        login: '',
-        password:'',
-        type:0
-      },
-      proxyTypes:['HTTP','Socks4', 'Socks5']
+      filterButton: {
+        loading: false,
+        progress: 0
+      }
     }),
     computed: {
       checkedItemsCount() {
@@ -207,13 +147,13 @@
       }
     },
     methods: {
-      getProxies()
+      getSmsList()
       {
         let vm = this;
         this.allSelected = false;
         this.isLoading = true;
         let new_items = [];
-        let url = '/api/settings/proxies/?page=' + this.page + '&itemsPerPage=' + this.itemsPerPage;
+        let url = '/api/sms-mms-messages/sms/?page=' + this.page + '&itemsPerPage=' + this.itemsPerPage;
         axios.get(url).then( response => {
           if (!response.data.errorCode )
           {
@@ -229,134 +169,43 @@
           vm.isLoading = false;
         });
       },
-
-      setProxiesStatus(id = null, status = null)
+      filter()
       {
-        this.hideDropDown(id);
-        this.isLoading = true;
+        this.filterButton.loading = true;
+        updateButtonProgress(duration, this.filterButton);
         let vm = this;
-        let url;
-        let data = {
-          value:status
-        };
-
-        if (id)
-          url = '/api/settings/proxies/' + id + '/status/';
-        else
-        {
-          url = '/api/settings/proxies/status/';
-          data.ids = this.items.filter(v=>v.checked).map(v=>v.id);
-        }
-
-        axios.patch(url,data).then( response => {
-          if (!response.data.errorCode )
-          {
-            this.$toast('Прокси успешно ' + (status ? 'активирован' : 'деактивирован') );
-            this.getProxies();
-
-          } else {
-            this.isLoading = false;
-            this.$toast(response.data.message, 'danger');
-          }
-
-        }).catch( responce => {
-          this.isLoading = false;
-        });
-      },
-
-      hideDropDown(id=null)
-      {
-        if (id)
-          this.$refs['dropdown_'+id].hide(true);
-        else {
-          this.$refs['dropdown0'].hide(true);
-          this.$refs['dropdown1'].hide(true);
-        }
-
-      },
-
-      selectAll()
-      {
-        this.allSelected = !this.allSelected;
-        for (let i in  this.items )
-          this.items[i].checked = this.allSelected;
-      },
-      deleteProxies(id = null)
-      {
-        this.hideDropDown(id);
-        this.isLoading = true;
-        let vm = this;
-        let url;
-        let data = {
-          value:1
-        };
-
-        if (id)
-          url = '/api/settings/proxies/' + id + '/';
-        else
-        {
-          url = '/api/settings/proxies/';
-          data.ids = this.items.filter(v=>v.checked).map(v=>v.id);
-        }
-
-        axios.delete(url,{data}).then( response => {
-          if (!response.data.errorCode )
-          {
-            this.page = 1;
-            this.$toast('Прокси успешно удален!');
-            this.getProxies();
-          } else {
-            this.$toast(response.data.message, 'danger');
-            this.isLoading = false;
-          }
-
-        }).catch( responce => {
-          this.isLoading = false;
-          this.$toast('При удалении возникли пробелмы', 'danger');
-        });
-      },
-      onDeleteProxies(id = null)
-      {
-        let title = !id ? 'Вы уверены в том что хотите удалить выделенные прокси?' : 'Вы уверены в том что хотите удалить прокси?';
-        this.$bvModal.msgBoxConfirm(title, {
-          title: 'Потвердите',
-          size: 'sm',
-          buttonSize: 'sm',
-          okVariant: 'danger',
-          okTitle: 'Да',
-          cancelTitle: 'Нет',
-          headerClass: 'p-2 border-bottom-0',
-          footerClass: 'p-2 border-top-0',
-          centered: false,
-          hideBackdrop : true,
-          noCloseOnBackdrop: true
-        })
-                .then(value => {
-                  if ( value )
-                    this.deleteProxies(id);
-                })
-                .catch(err => {
-
-                })
-      },
-      onEditProxy(proxy)
-      {
-        this.proxyToEdit = proxy;
-        this.showEditProxyModal = true;
+        setTimeout(function () { vm.filterButton.loading = false;}, duration);
       }
     },
+
     watch: {
       itemsPerPage : function(val)
       {
-        this.$cookies.set('proxies_list_per_page', val, '31d');
+        this.$cookies.set('sms_messages_list_per_page', val, '31d');
       }
     },
     mounted()
     {
-      let itemsPerPage = this.$cookies.get('proxies_list_per_page');
+      let itemsPerPage = this.$cookies.get('sms_messages_list_per_page');
       if (itemsPerPage && parseInt(itemsPerPage)>0)
         this.itemsPerPage = itemsPerPage;
-      this.getProxies();
+      this.getSmsList();
     }
+  }
+
+  function updateButtonProgress(duration, button) {
+    var start = null;
+
+    function update(timestamp) {
+      var delta, progress;
+      if (!start) start = timestamp;
+      delta = timestamp - start;
+      progress = delta / duration;
+      if (progress >= 1 || progress < 0) return;
+      button.progress = progress;
+      window.requestAnimationFrame(update);
+    }
+
+    window.requestAnimationFrame(update);
   }
 </script>
