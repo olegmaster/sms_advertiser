abb12b9596d04b96b045603eafba9fbab95a6474
Oleg Pristashkin <oleg.web.pro@gmail.com>
2020-07-31 16:18:38 +0300

    add simbank model constants

--- app/Models/AdvertisingCampaign/Simbank.php
+++ app/Models/AdvertisingCampaign/Simbank.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Simbank extends Model
 {
-    //
+    public const ACTIVATE_ACTION = 1;
+    public const DEACTIVATE_ACTION = 2;
+    public const UPDATE_SIMBANK_DATA_ACTION = 3;
+
+    public const ACTIVE_STATUS = 1;
+    public const INACTIVE_STATUS = 0;
+
+
+
+
 }
