5da4f5b7acce0594314a3e1d6691981eb02f4c87
Oleg Pristashkin <oleg.web.pro@gmail.com>
2020-07-31 15:16:55 +0300

    delete unused actions from the domainsRedirectsController

--- app/Http/Controllers/Api/Settings/DomainsRedirectsController.php
+++ app/Http/Controllers/Api/Settings/DomainsRedirectsController.php
@@ -35,16 +35,6 @@ class DomainsRedirectsController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param \Illuminate\Http\Request $request
@@ -67,27 +57,6 @@ class DomainsRedirectsController extends Controller
         }
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
 
     /**
      * Update the specified resource in storage.
