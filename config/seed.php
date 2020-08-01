2d3f34a88423494110d040cafc4512ca9e154c16
Sherzod <fortuneman@list.ru>
2020-08-01 13:15:43 +0500

    Seed config changed

--- config/seed.php
+++ config/seed.php
@@ -3,7 +3,7 @@
 return [
     'countriesCount' => env('SEED_COUNTRIES_COUT', 3),
     'usersCount' => env('SEED_USERS_COUNT', 10),
-    'thematicsCount' => env('SEED_THEMATICS_COUNT', 100),
+    'thematicsCount' => env('SEED_THEMATICS_COUNT', 20),
 
     // sim
     'simbanksCount' => env('SEED_SIMBANKS_COUNT', 4),
