ada688eff042f3c4f9c9830e5958819e8d480715
Sherzod <fortuneman@list.ru>
2020-08-02 12:59:20 +0500

    Seeder BoardsTableSeeder added

--- app/Models/Targeting/Board.php
+++ app/Models/Targeting/Board.php
@@ -2,9 +2,14 @@
 
 namespace App\Models\Targeting;
 
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Model,
+    App\Traits\RealPagination;
 
 class Board extends Model
 {
-    //
+    use RealPagination;
+
+    protected $table = 'boards';
+
+    public $timestamps = true;
 }
